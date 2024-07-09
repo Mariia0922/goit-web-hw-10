4619f40c2afc3ea049ebc27df451e682c611a888
mariia <mashakeyt2209@gmail.com>
2024-07-09 15:44:31 +0200

    first commit

---
+++ quotes_project/quotes_app/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import AuthorForm, QuoteForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'quotes_app/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'quotes_app/login.html', {'form': form})
+
+@login_required
+def add_author(request):
+    if request.method == 'POST':
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = AuthorForm()
+    return render(request, 'quotes_app/add_author.html', {'form': form})
+
+@login_required
+def add_quote(request):
+    if request.method == 'POST':
+        form = QuoteForm(request.POST)
+        if form.is_valid():
+            quote = form.save(commit=False)
+            quote.added_by = request.user
+            quote.save()
+            return redirect('home')
+    else:
+        form = QuoteForm()
+    return render(request, 'quotes_app/add_quote.html', {'form': form})
